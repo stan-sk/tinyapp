9db535cc77bcccc2cbd6e505a1ff23b5af78318b
Stanley Lai <stanley.lai@telus.com>
2022-11-28 15:46:57 -0800

    adding 2nd route /urls/:id

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id: req.params.id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
